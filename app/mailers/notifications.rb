909011f62f2afa99d01ba0ae7c430334598ab6a2
Erik Douglas <eadouglas10@gmail.com>
2016-08-07 19:33:04 -0400

    Added mailer

---
+++ app/mailers/notifications.rb
@@ -0,0 +1,13 @@
+class Notifications < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifications.signup.subject
+  #
+  def signup
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
